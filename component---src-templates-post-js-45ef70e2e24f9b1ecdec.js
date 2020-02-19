3327fe405b631989341a5b7588ec31b7ac5892a6
YagoLopes <yagolopeslazaro@gmail.com>
2020-02-18 21:22:36 -0300

    Updates

--- component---src-templates-post-js-45ef70e2e24f9b1ecdec.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{"6qSS":function(e,t,n){"use strict";n.r(t),n.d(t,"query",(function(){return l}));var r=n("q1tI"),a=n.n(r);t.default=function(e){var t=e.data.markdownRemark,n={__html:t.html};return a.a.createElement("div",null,a.a.createElement("div",null,t.frontmatter.title),a.a.createElement("div",{dangerouslySetInnerHTML:n}))};var l="2559881278"}}]);
-//# sourceMappingURL=component---src-templates-post-js-45ef70e2e24f9b1ecdec.js.map
