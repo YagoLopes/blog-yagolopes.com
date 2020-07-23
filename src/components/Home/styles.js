92a7cc0145826f5e2132ed3471bd12cc315be415
Yago Lopes <yagolopeslazaro@gmail.com>
2020-07-23 12:29:11 -0300

    max-width select post

--- src/components/Home/styles.js
+++ src/components/Home/styles.js
@@ -9,12 +9,12 @@ export const Container = styled.div`
 `
 
 export const PostList = styled.div`
- margin: 15px 0;
- padding: 5%;
+ margin: 5px 0;
+ padding: 10px;
  overflow-y: scroll;
  min-width: 50%;
  min-height: 40%;
- min-height: 50%;
+ max-height: 50%;
  border-radius: 3px;
  border: 1px solid rgba(0, 0, 0, 0.3);
  background-color: rgba(0, 0, 0, 0.1);
